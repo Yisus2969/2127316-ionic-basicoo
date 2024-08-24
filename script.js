1f45bbca96fcbfd92181ff689df618bc5cef0704
Yisus2969 <jesusangel2969@gmail.com>
2024-08-24 10:06:26 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function imprimirTexto() {
+  var campoTexto = document.getElementById("campoTxt");
+  var texto = campoTexto.value;
+
+  console.log(texto);
+}
+function borrarTexto() {
+  var campoTexto = document.getElementById("campoTxt");
+  campoTexto.value = "";
+
+  console.log(texto);
+}
