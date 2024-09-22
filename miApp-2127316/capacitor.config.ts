d64f6823d4eef0f58a770cf3882aee09e1ab797f
Yisus2969 <jesusangel2969@gmail.com>
2024-09-21 22:55:51 -0600

    Primer commit de miApp-2127316

---
+++ miApp-2127316/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miApp-2127316',
+  webDir: 'www'
+};
+
+export default config;
