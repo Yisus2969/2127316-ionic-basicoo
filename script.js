a1907ff746e8984124567333dac41a41e6a3597e
Yisus2969 <jesusangel2969@gmail.com>
2024-10-06 12:19:30 -0600

    borrando script

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-var usuarioTxt = document.getElementById("usuarioTxt");
-var passwordTxt = document.getElementById("passwordTxt");
 
-var parrafo = document.querySelector("p");
-
-function iniciarSesion() {
-  var usuarioValor = usuarioTxt.value;
-  parrafo.innerHTML = `Registro con éxito ${usuarioValor}`;
-}
-
-function registro() {
-  var usuarioValor = usuarioTxt.value;
-  parrafo.innerHTML = "Registro con exito" + usuarioValor;
-}
